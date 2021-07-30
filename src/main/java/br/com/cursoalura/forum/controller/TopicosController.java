2d872845ca1f4cbc4d1cfd47c48c6c5f9927926d
Rafaela Lima Nogueira <rafaela.nogueira@meta.com.br>
2021-07-29 23:08:36 -0300

    Implementado o @BeanValidation

--- src/main/java/br/com/cursoalura/forum/controller/TopicosController.java
+++ src/main/java/br/com/cursoalura/forum/controller/TopicosController.java
@@ -1,24 +1,38 @@
 package br.com.cursoalura.forum.controller;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.cursoalura.forum.model.Curso;
 import br.com.cursoalura.forum.model.Topico;
 import br.com.cursoalura.forum.repository.TopicoRepository;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
+    @Autowired
+    private CursoRepository cursoRepository;
     
-    @RequestMapping("/topicos")
+    @GetMapping
     public List<TopicoDTO> lista(String nomeCurso) {
 
         if (Objects.isNull(nomeCurso)) {
@@ -29,4 +43,14 @@ public class TopicosController {
             return TopicoDTO.converter(topicos);
         }
     }
+
+    @PostMapping
+    public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
+        Topico topico = form.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("/topicos/[id]").buildAndExpand(topico.getId()).toUri(); //
+        return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+    }
+    
 }
