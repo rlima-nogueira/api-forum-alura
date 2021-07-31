c5eecaaef5b500511557a69cc3aaeceebd0df7de
Rafaela <rn-15@hotmail.com>
2021-07-31 09:55:20 -0300

    Utilizando o recurso Pageable para a paginacao

--- src/main/java/br/com/cursoalura/forum/controller/TopicosController.java
+++ src/main/java/br/com/cursoalura/forum/controller/TopicosController.java
@@ -9,9 +9,15 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -45,13 +51,14 @@ public class TopicosController {
     
     @ApiOperation(value = "Mostra todos os tópicos")
     @GetMapping
-    public List<TopicoDTO> lista(String nomeCurso) {
-
+    public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, 
+       @PageableDefault(sort="id", direction= Direction.DESC, page = 0, size = 10) Pageable paginacao) {
+    
         if (Objects.isNull(nomeCurso)) {
-            List<Topico> topicos = topicoRepository.findAll();
+            Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDTO.converter(topicos);
         } else {
-            List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+            Page<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso, paginacao);
             return TopicoDTO.converter(topicos);
         }
     }
