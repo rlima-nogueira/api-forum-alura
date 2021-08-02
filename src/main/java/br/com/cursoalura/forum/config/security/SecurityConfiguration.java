d33968e4c219c873b6e2e941206b559b463bbdc1
Rafaela <rn-15@hotmail.com>
2021-08-01 22:09:23 -0300

    Criado os metodos de autenticacao pelo username

--- src/main/java/br/com/cursoalura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/cursoalura/forum/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.cursoalura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,17 +8,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;;
     
     /**
      * Configurações de autenticação
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     /**
@@ -27,7 +32,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-            .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+            .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+            .anyRequest().authenticated()
+            .and().formLogin();
     }
 
     /**
@@ -35,6 +42,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
      */
     @Override
     public void configure(WebSecurity web) throws Exception {
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
     }
 
 }
